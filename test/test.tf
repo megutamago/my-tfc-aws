c97deaf6c1d1e37efef9b7ce862faa24e1e5e2e7
megutamago <naoya05280708@gmail.com>
2023-08-13 12:27:54 +0900

    submit

--- test/test.tf
+++ test/test.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
